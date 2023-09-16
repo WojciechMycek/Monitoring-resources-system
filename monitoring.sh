c4c2254ab2e508f5d3b79032dadc9301915161d1
Wojciech Mycek <wojciech.mycek97@gmail.com>
2023-09-16 19:40:39 +0200

    Defined function to check disk size

--- monitoring.sh
+++ monitoring.sh
@@ -12,8 +12,22 @@ disk_check_file(){
 	argument=$(awk '{if(NR == 2) {print $3}}' disk.txt)
 	echo "Disk size is $argument"
 
+	#Define str to int from disk size
+
+	blank=""
+	point="."
+
+	redefined_argument=$(echo "$argument" | sed "s/M/$blank/g")
+	echo "Deleted M $redefined_argument"
+
+	ready_argument=$(echo "$redefined_argument" | sed "s/,/$point/g")
+	echo "Argument after clean-up $ready_argument"
+
+	#TRANSFER STR TO INT
+	float_argument=$(echo "$ready_argument" | bc)
+
 	#check disk size
-	if [ $argument -gt 2 ]
+	if [ $float_argument > 2 ]
 	then
 		echo "ERROR: DISK SIZE EXCEDDED!!!"
 	fi
