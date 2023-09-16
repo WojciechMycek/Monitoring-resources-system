6a6fcf44ee0b4864a3cfcd7fccc33ef87cd2ff72
Wojciech Mycek <wojciech.mycek97@gmail.com>
2023-09-16 19:23:51 +0200

    Defined way to get disk size

--- monitoring.sh
+++ monitoring.sh
@@ -1,2 +1,23 @@
-df -h > disk.txt
-cat disk.txt
+disk_check_file(){	
+		
+	#defining disk check
+
+	df -h > disk.txt
+	cat disk.txt
+	grep -c "tm" disk.txt
+	awk '{if(NR == 2) {print $3}}' disk.txt > status.txt
+	cat status.txt
+
+	#Writing disk size to argument
+	argument=$(awk '{if(NR == 2) {print $3}}' disk.txt)
+	echo "Disk size is $argument"
+
+	#check disk size
+	if [ $argument -gt 2 ]
+	then
+		echo "ERROR: DISK SIZE EXCEDDED!!!"
+	fi
+
+}
+
+disk_check_file
